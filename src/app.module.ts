1832aad96ee8008e3c212bc43045bb3c20dd1970
KostyalBalint <kostyal.balintnak@gmail.com>
2023-02-23 20:46:06 +0100

    feat: add healthcheck and db schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Config } from './config/config';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
+    HealthModule,
+    Config,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
     }),
-    Config,
   ],
 })
 export class AppModule {}
