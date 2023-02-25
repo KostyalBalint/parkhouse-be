2fefe77351a9c57895723fcded5391425229bb19
Loznek <#Darius44>
2023-02-25 17:15:01 +0100

    Update seeder

--- src/prisma/seed/seed.ts
+++ src/prisma/seed/seed.ts
@@ -1,26 +1,55 @@
-import { PrismaClient } from '@prisma/client';
+import { ParkingSpaceType, Prisma, PrismaClient, User } from '@prisma/client';
 import { faker } from '@faker-js/faker';
-import { User } from '@prisma/client';
+import { ParkingSpaceStatus } from '../../graphql/graphqlTypes';
 
 const prisma = new PrismaClient();
 
-function seedUsers({ count }: { count: number }) {
-  const fakerUser = (): Omit<User, 'id'> => ({
-    name: faker.name.firstName(),
+async function seedUsers({ count }: { count: number }) {
+  const names = new Set<string>();
+  while (names.size < count) {
+    names.add(faker.name.firstName());
+  }
+  const fakeNames = Array.from(names);
+
+  const fakerUser = (index: number): Omit<User, 'id'> => ({
+    name: fakeNames[index],
     avatar: faker.image.avatar(),
     phoneNumber: faker.phone.number('+36-30-###-####'),
     password: 'password',
-    coin: 123,
     selectedGameCarId: null,
+    coin: 123,
+  });
+  await prisma.user.create({
+    data: {
+      id: 'admin',
+      name: 'Admin',
+      avatar: faker.image.avatar(),
+      phoneNumber: faker.phone.number('+36-30-###-####'),
+      password: 'password',
+      selectedGameCarId: null,
+      coin: 123,
+    },
   });
   return prisma.user.createMany({
-    data: Array.from({ length: count }).map(fakerUser),
+    data: Array.from({ length: count }).map((_, index) => fakerUser(index)),
   });
+}
+function generatePlate() {
+  let plate = '';
+  for (let i = 0; i < 3; i++) {
+    plate += String.fromCharCode(Math.floor(65 + Math.random() * 26));
+  }
+  plate += '-';
+  for (let i = 0; i < 3; i++) {
+    plate += String(Math.floor(Math.random() * 10));
+  }
+
+  return plate;
 }
 
 async function seedCars({ users }: { users: User[] }) {
   const cars = users.map((user) => ({
-    licencePlate: faker.vehicle.vin(),
+    licencePlate: generatePlate(),
     userId: user.id,
     name: faker.vehicle.model(),
   }));
@@ -29,31 +58,68 @@ async function seedCars({ users }: { users: User[] }) {
   });
 }
 
-function seedLevels({ count }: { count: number }) {
-  return prisma.level.createMany({
-    data: Array.from({ length: count }).map(() => ({
-      label:
-        faker.random.alpha({ count: 2 }) + faker.random.numeric(2).toString(),
-    })),
+async function seedLevels(count: number, users: User[]) {
+  const parkingSpaces = (spaceIndex: number, levelIndex: number) => ({
+    label: `A-${levelIndex * 100 + spaceIndex}`,
+    type: ParkingSpaceType.NORMAL,
+    currentStatus: ParkingSpaceStatus.FREE,
   });
+
+  const parkingOwnedSpaces = (
+    spaceIndex: number,
+    users: User[],
+    levelIndex: number,
+  ) => ({
+    label: `A-${levelIndex * 100 + spaceIndex}`,
+    type: ParkingSpaceType.NORMAL,
+    ownerId: users[spaceIndex].id,
+    currentStatus: ParkingSpaceStatus.RESERVED_FOR_OWNER,
+  });
+  const levels = (index: number): Omit<Prisma.LevelCreateInput, 'id'> => ({
+    label: `${index}.emelet`,
+    ParkingSpace: {
+      createMany: {
+        data: [
+          ...Array.from({ length: 14 }).map((_, spaceIndex) =>
+            parkingOwnedSpaces(
+              spaceIndex,
+              users.slice(14 * index, 14 * (index + 1)),
+              index,
+            ),
+          ),
+          ...Array.from({ length: 2 }).map((_, spaceIndex) =>
+            parkingSpaces(spaceIndex, index),
+          ),
+        ],
+      },
+    },
+  });
+  for (let i = 0; i < count; i++) {
+    await prisma.level.create({
+      data: levels(i),
+    });
+  }
+  /*
+  return prisma.level.createMany({
+    data: Array.from({ length: count }).map((_, index) => levels(index)),
+  });*/
 }
 
 async function main() {
-  const users = await seedUsers({
-    count: 10,
+  await seedUsers({
+    count: 100,
   });
 
-  console.log('Seeded users:', users.count);
-
+  const users = await prisma.user.findMany();
+  console.log('Seeded users:', users.length);
   const cars = await seedCars({
     users: await prisma.user.findMany(),
   });
   console.log('Seed cars:', cars.count);
 
-  const levels = await seedLevels({
-    count: 10,
-  });
-  console.log('Seeded levels:', levels.count);
+  await seedLevels(5, users);
+  const levels = await prisma.level.findMany();
+  console.log('Seed levels:', levels.length);
 }
 
 main()
